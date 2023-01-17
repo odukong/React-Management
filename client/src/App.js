8e13ebf9b3c4f2cb340bcc39736521cbefc80f54
odukong <ohbin1017@gmail.com>
2023-01-18 02:21:27 +0900

    Update UI/UX

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import './index.css'
 import Customer from './components/Customer';
 import CustomerAdd from "./components/CustomerAdd";
 import Paper from "@material-ui/core/Paper";
@@ -11,19 +12,80 @@ import { withStyles } from '@material-ui/core';
 import { useEffect,useState } from 'react';
 import CircularisLoad from '@material-ui/core/CircularProgress';
 
+import AppBar from '@mui/material/AppBar';
+import { styled, alpha } from '@mui/material/styles';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import InputBase from '@mui/material/InputBase';
+import MenuIcon from '@mui/icons-material/Menu';
+import SearchIcon from '@mui/icons-material/Search';
+import Box from '@mui/material/Box';
+
 const styles=(theme)=>({
   root:{
     width:'100%',
-    marginTop:theme.spacing(3),
-    overflow:"auto"
-  },
-  table:{
     minWidth:1080
+  },
+  paper:{
+    marginLeft:18,
+    marginRight:18,
+  },
+  tableHead:{
+    fontSize:'1.0rem'
+  },
+  menu:{
+    marginTop:15,
+    marginBottom:15,
+    display:'flex',
+    justifyContent:'center'
   },
   isLoad:{
     margin:theme.spacing(2)
   }
 })
+
+const Search = styled('div')(({ theme }) => ({
+  position: 'relative',
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  '&:hover': {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginLeft: 0,
+  width: '100%',
+  [theme.breakpoints.up('sm')]: {
+    marginLeft: theme.spacing(1),
+    width: 'auto',
+  },
+}));
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: '100%',
+  position: 'absolute',
+  pointerEvents: 'none',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: 'inherit',
+  '& .MuiInputBase-input': {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      width: '12ch',
+      '&:focus': {
+        width: '20ch',
+      },
+    },
+  },
+}));
 
 const serverURL="http://localhost:5000/api/customers";
 
@@ -63,19 +125,55 @@ function App(props) {
       },[])
 
       const {classes}=props;
+      const cellList=["번호","이미지","이름","생년월일","성별","직업","설정"];
       return (
-        <div>
-          <Paper className={classes.root}>
+        <div className={classes.root}>
+          <Box sx={{ flexGrow: 1 }}>
+            <AppBar position="static">
+              <Toolbar>
+                <IconButton
+                  size="large"
+                  edge="start"
+                  color="inherit"
+                  aria-label="open drawer"
+                  sx={{ mr: 2 }}
+                >
+                  <MenuIcon />
+                </IconButton>
+                <Typography
+                  variant="h6"
+                  noWrap
+                  component="div"
+                  sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
+                >
+                  고객 관리 시스템
+                </Typography>
+                <Search>
+                  <SearchIconWrapper>
+                    <SearchIcon />
+                  </SearchIconWrapper>
+                  <StyledInputBase
+                    placeholder="검색하기"
+                    inputProps={{ 'aria-label': 'search' }}
+                  />
+                </Search>
+              </Toolbar>
+            </AppBar>
+          </Box>
+          <div className={classes.menu}>
+            <CustomerAdd stateRefresh={stateRefresh}/>
+          </div>
+          <Paper className={classes.paper}>
             <Table className={classes.table}>
               <TableHead>
                 <TableRow>
-                  <TableCell>번호</TableCell>
-                  <TableCell>이미지</TableCell>
-                  <TableCell>이름</TableCell>
-                  <TableCell>생년월일</TableCell>
-                  <TableCell>성별</TableCell>
-                  <TableCell>직업</TableCell>
-                  <TableCell>설정</TableCell>
+                  {cellList.map((i)=>{
+                    return(
+                      <TableCell className={classes.tableHead}>
+                        {i}
+                      </TableCell>
+                      );
+                    })}
                 </TableRow>
               </TableHead>
               <TableBody>
@@ -103,7 +201,6 @@ function App(props) {
               </TableBody>
             </Table>
           </Paper>
-          <CustomerAdd stateRefresh={stateRefresh}/>
         </div>
       );
 }
